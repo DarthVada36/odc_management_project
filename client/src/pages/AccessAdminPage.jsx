9018c463e254766cddcbe9a603fcd0a2ae311410
jruizndev <jorsqn@gmail.com>
2024-12-07 17:59:57 +0100

    feat: implement route protection and navigation controls - Add ProtectedRoute component for secure route access - Configure protected routes for dashboard and admin management - Adjust nav menu items based on routes and authentication - Update navigation logic in nav component  * Show Dashboard label only in admin routes  * Configure Dashboard label activation state  * Handle navigation to dashboard  * Remove login icon and update logout redirection  * Hide menu items in login page

--- client/src/pages/AccessAdminPage.jsx
+++ client/src/pages/AccessAdminPage.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import AccessForm from '../components/AccessForm'; 
+import React from 'react'
+import AccessForm from '../components/AccessForm'
 
 const AccessAdminPage = () => {
-  return (
-    <div className="flex min-h-screen">
-      {/* Imagen de la izquierda */}
-      <div className="w-1/2 bg-cover bg-center" style={{ backgroundImage: 'url(/img-login.png)' }}>
-      </div>
+    return (
+        <div className="flex min-h-screen">
+            {/* Imagen de la izquierda */}
+            <div
+                className="w-1/2 bg-center bg-cover"
+                style={{ backgroundImage: 'url(/img-login.png)' }}
+            ></div>
 
-      {/* Formulario de acceso a la derecha */}
-      <div className="w-1/2 bg-white p-8">
-     
+            {/* Formulario de acceso a la derecha */}
+            <div className="w-1/2 p-8 bg-white">
                 <AccessForm />
-      </div>
-    </div>
-  );
-};
+            </div>
+        </div>
+    )
+}
 
-export default AccessAdminPage;
+export default AccessAdminPage
