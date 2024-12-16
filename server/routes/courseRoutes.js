f397bbb1181e8865bc5e47fc9a5dd2d808ba838e
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-12-16 13:20:42 +0100

    feature-database merge with dev

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -5,30 +5,13 @@ import {
     createCourse,
     getStudentsByCourse,
     updateCourse,
-<<<<<<< HEAD
-    deleteCourseFromDb    
-} from '../controllers/coursesController.js';
-import { checkRol } from '../middleware/rolMiddleware.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
-import { validateCreateCourse,validateUpdateCourse } from '../utils/validations/courseValidation.js';
-import  validationHandler from '../utils/handle/handleValidator.js';
-=======
     deleteCourseFromDb,
 } from '../controllers/coursesController.js'
 import { checkRol } from '../middleware/rolMiddleware.js'
 import { authMiddleware } from '../middleware/authMiddleware.js'
->>>>>>> dev
 
 const courseRoutes = express.Router()
 
-<<<<<<< HEAD
-courseRoutes.get('/',  getAllCourses);
-courseRoutes.get('/:id',  getCourseById);
-courseRoutes.get('/:id/students', getStudentsByCourse);
-courseRoutes.post('/', validateCreateCourse, validationHandler, authMiddleware, checkRol(['superadmin', 'admin']), createCourse);
-courseRoutes.put('/:id', validateUpdateCourse, validationHandler, updateCourse);
-courseRoutes.delete('/:id', deleteCourseFromDb);
-=======
 courseRoutes.get('/', getAllCourses)
 courseRoutes.get('/:id', getCourseById)
 courseRoutes.get('/:id/students', getStudentsByCourse)
@@ -40,6 +23,5 @@ courseRoutes.post(
 )
 courseRoutes.put('/:id', updateCourse)
 courseRoutes.delete('/:id', deleteCourseFromDb)
->>>>>>> dev
 
 export default courseRoutes
